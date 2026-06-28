402cbd44131fce3cedb9b1e63fa2ee279f9bb1f7
Satyam Kumar <128212757+unstoppable-satyam@users.noreply.github.com>
2026-06-28 15:13:47 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1) return list2;
+        if(!list2) return list1;
+
+        if(list1->val <= list2->val ){
+            list1->next = mergeTwoLists(list1->next , list2);
+            return list1;
+        }
+        else{
+            list2->next = mergeTwoLists(list1 , list2->next);
+            return list2;
+        }
+
+
+        // if(list1 == nullptr && list2 == nullptr) return list1;
+        // if(list1 == nullptr) return list2;
+        // if(list2 == nullptr) return list1;
+
+        // ListNode* dummy = new ListNode(-1);
+        // ListNode* prev_head = dummy;
+        // while(  (list1!= NULL) && (list2!= NULL ) ){
+        //     if( list1->val <= list2->val) {
+        //         dummy->next= list1;
+        //         list1 = list1->next;
+        //     }
+        //     else{
+        //         dummy->next = list2;
+        //         list2 = list2->next; 
+        //     }
+        //     dummy = dummy->next;
+        // }
+        // while( (list1!= NULL) ){
+        //     dummy->next= list1;
+        //     list1 = list1->next;
+        //     dummy = dummy->next;
+        // }
+        // while( (list2!= NULL) ){
+        //     dummy->next= list2;
+        //     list2 = list2->next;
+        //     dummy = dummy->next;
+        // }
+        // return prev_head->next;
+    }
+};
